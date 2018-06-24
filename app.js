32671b5f2a1fff2fceaa5362d2353de9901fc5d2
Alexandre Beaulieu <alex@segfault.me>
2018-06-24 21:51:55 +0000

    ...?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const helmet = require('helmet')
-const passport = require('passport')
-const auth = require('./router/auth');
+// const passport = require('passport')
+// const auth = require('./router/auth');
 const app = express()
 
 app.use(helmet());
@@ -14,23 +14,23 @@ app.use(helmet.contentSecurityPolicy({
 app.use(helmet.referrerPolicy({ policy: 'no-referrer' }))
 // app.use(helmet.expectCt({ enforce: true, maxAge: 100 }));
 
-if (app.get('env') === 'development' || process.env.NODE_DEBUG == 'true'){
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({
-      message: err.message,
-      error: err
-    });
-  });
-} else {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({
-      message: err.message,
-      error: {}
-    });
-  });
-}
+// if (app.get('env') === 'development' || process.env.NODE_DEBUG == 'true'){
+//   app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     res.send({
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// } else {
+//   app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     res.send({
+//       message: err.message,
+//       error: {}
+//     });
+//   });
+// }
 
 app.get('/', (req, res) => {
   res.send('WIP');
